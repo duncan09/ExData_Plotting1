bbf55d94845abcb7fdf08b7d98c9aef94efe138c
duncan09 <dunmugambi@gmail.com>
2016-06-26 13:54:59 -0700

    Update plot1.R

--- figure/plot1.R
+++ figure/plot1.R
@@ -1,7 +1,7 @@
-+Irvinedata <- "./data/household_power_consumption.txt"
-+Irvinedata <- read.table(Irvinedata, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-+subsetUC <- Irvinedata[Irvinedata$Date %in% c("1/2/2007","2/2/2007") ,]
-+globalActivePower <- as.numeric(subsetUC$Global_active_power)
-+png("plot1.png", width=480, height=480)
-+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
-+dev.off()
+UCdata <- "./data/household_power_consumption.txt"
+UCdata <- read.table(UCdata, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subsetUC <-UCdata[UCdata$Date %in% c("1/2/2007","2/2/2007") ,]
+globalActivePower <- as.numeric(subsetUC$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
